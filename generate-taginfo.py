05624da051ee041944849489830fd52d93f1a6dc
Wieland <29458682+wielandb@users.noreply.github.com>
2023-02-10 13:19:54 +0100

    Change the way changes in the source material are detected

--- generate-taginfo.py
+++ generate-taginfo.py
@@ -2,39 +2,25 @@ import os, sys, json
 from datetime import datetime
 import urllib.request
 import hashlib
+import requests
 
 TAGINFO = {}
-
-# before we change anything, we need to make sure that something changed
-# if nothing changed, we don't want to create a new commit
-
-# load the file "taginfo.json"
-with open("taginfo.json", "r", encoding="utf8") as f:
-	taginfo = json.loads(f.read())
-
-# read the md5 of the previous taginfo.json
-previous_md5 = taginfo["project"]["catalog-md5"] 
-
-
-
-
-# Set up the basic parameters
-TAGINFO["data_format"] = 1
-TAGINFO["data_url"] = "https://raw.githubusercontent.com/OPENER-next/OpenStop-taginfo/main/taginfo.json"
-TAGINFO["project"] = {
-	"name": "OpenStop",
-	"description": "App for collecting OpenStreetMap-compliant accessibility data in public transport",
-	"project_url": "https://openstop.app/",
-	"doc_url": "https://github.com/OPENER-next/OpenStop",
-	"icon_url": "https://raw.githubusercontent.com/OPENER-next/OpenStop/master/web/favicon.png",
-	"contact_name": "OPENER next",
-	"contact_email": "openernext@etit.tu-chemnitz.de"
-}
-TAGINFO["tags"] = []
 
 TAGS = []
 
 questions_for_tags = {}
+
+def get_last_commit_date(repo, path):
+    url = f"https://api.github.com/repos/{repo}/commits?path={path}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        last_commit = data[0]
+        last_commit_date = last_commit["commit"]["committer"]["date"]
+        return last_commit_date
+    else:
+        raise Exception("Failed to retrieve commit information for the file.")
+
 
 def AddToQuestionsForTags(key, value, question, elementtypes):
 	global questions_for_tags
@@ -79,109 +65,100 @@ def OpenStopTypeToTaginfoType(s):
 	d = {"OpenWay": "way", "ClosedWay": "area", "Node":"node", "Relation":"relation"}
 	return d[s]
 
+# before we change anything, we need to make sure that something changed
+# if nothing changed, we don't want to create a new commit
 
 
-# the directory where the .json-Files are located
-json_dir = ""
+def main():
+	global TAGS
+	global questions_for_tags
+	# load the file "taginfo.json"
+	with open("taginfo.json", "r", encoding="utf8") as f:
+		taginfo = json.loads(f.read())
 
-# load the files "map_feature_collection.json" and "question_catalog.json"
-# Download the files from GitHub
-urllib.request.urlretrieve("https://raw.githubusercontent.com/OPENER-next/OpenStop/master/assets/datasets/map_feature_collection.json", os.path.join(json_dir, "map_feature_collection.json"))
-urllib.request.urlretrieve("https://raw.githubusercontent.com/OPENER-next/OpenStop/master/assets/datasets/question_catalog.json", os.path.join(json_dir, "question_catalog.json"))
+	# read the md5 of the previous taginfo.json
+	previous_md5 = taginfo["project"]["catalog-md5"] 
 
-# load the file "map_feature_collection.json"
-with open(os.path.join(json_dir, "map_feature_collection.json")) as f:
-	map_feature_collection = json.load(f)
+	# Set up the basic parameters
+	TAGINFO["data_format"] = 1
+	TAGINFO["data_url"] = "https://raw.githubusercontent.com/OPENER-next/OpenStop-taginfo/main/taginfo.json"
+	TAGINFO["project"] = {
+		"name": "OpenStop",
+		"description": "App for collecting OpenStreetMap-compliant accessibility data in public transport",
+		"project_url": "https://openstop.app/",
+		"doc_url": "https://github.com/OPENER-next/OpenStop",
+		"icon_url": "https://raw.githubusercontent.com/OPENER-next/OpenStop/master/web/favicon.png",
+		"contact_name": "OPENER next",
+		"contact_email": "openernext@etit.tu-chemnitz.de"
+	}
+	TAGINFO["tags"] = []
 
-# load the file "question_catalog.json"
-with open(os.path.join(json_dir, "question_catalog.json"), "r", encoding="utf8") as f:
-	question_catalog = json.load(f)
+	# the directory where the .json-Files are located
+	json_dir = ""
 
+	# load the files "map_feature_collection.json" and "question_catalog.json"
+	# Download the files from GitHub
+	urllib.request.urlretrieve("https://raw.githubusercontent.com/OPENER-next/OpenStop/master/assets/datasets/map_feature_collection.json", os.path.join(json_dir, "map_feature_collection.json"))
+	urllib.request.urlretrieve("https://raw.githubusercontent.com/OPENER-next/OpenStop/master/assets/datasets/question_catalog.json", os.path.join(json_dir, "question_catalog.json"))
 
+	# load the file "map_feature_collection.json"
+	with open(os.path.join(json_dir, "map_feature_collection.json")) as f:
+		map_feature_collection = json.load(f)
 
-# Deactivated for now, there is no real reason we want to display the *single* tags used to display the map features
-# If we would, why wouldn't we also want to display the tags used as conditions for the questions?
-"""
-# iterate over the map_feature_collection
-for feature in map_feature_collection:
-	for condition in feature["conditions"]:
-		# if condition["osm_element"] is not set, then object_types = ["node", "way", "relation"]
-		# if condition["osm_element"] is a single string, parse it and set object_types accordingly
-		# if condition["osm_element"] is a list of strings, parse it and set object_types accordingly
-		object_types = []
-		if "osm_element" not in condition:
-			object_types = ["node", "way", "relation"]
-		elif isinstance(condition["osm_element"], str):
-			if condition["osm_element"] == "Node":
-				object_types = ["node"]
-			elif condition["osm_element"] == "OpenWay":
-				object_types = ["way"]
-			elif condition["osm_element"] == "ClosedWay":
-				object_types = ["way"]
-			elif condition["osm_element"] == "Relation":
-				object_types = ["relation"]
-		# if object_types is still empty, then set it to ["node", "way", "relation", "area"]
-		if len(object_types) == 0:
-			object_types = ["node", "way", "relation", "area"]
-		for key, value in condition["osm_tags"].items():
-			AddToTags(key, value, object_types, "Used to identify and display \"" + str(feature["name"]) + "\" on the map.")
-"""
+	# load the file "question_catalog.json"
+	with open(os.path.join(json_dir, "question_catalog.json"), "r", encoding="utf8") as f:
+		question_catalog = json.load(f)
 
-
-
-# iterate over the question_catalog
-for question in question_catalog:
-	if "input" not in question["answer"]:
-		continue
-	# if there is a value "osm_element" in the condition, get the elements and set them as objecttypes
-	objecttypes = ["node", "way", "relation", "area"]
-	if "conditions" in question:
-		if "osm_element" in question["conditions"][0]:
-			objecttypes = []
-			# get what type of data is stored at question["conditions"][0]["osm_element"]
-			if isinstance(question["conditions"][0]["osm_element"], str):
-				objecttypes.append(OpenStopTypeToTaginfoType(question["conditions"][0]["osm_element"]))
-			elif isinstance(question["conditions"][0]["osm_element"], list):
-				for t in question["conditions"][0]["osm_element"]:
-					objecttypes.append(OpenStopTypeToTaginfoType(t))
-
-	for answer in question["answer"]["input"]:
-		if "osm_tags" not in answer:
+	# iterate over the question_catalog
+	for question in question_catalog:
+		if "input" not in question["answer"]:
 			continue
-		for key, value in answer["osm_tags"].items():
-			AddToQuestionsForTags(key, value, str(question["question"]["text"]), objecttypes)
+		# if there is a value "osm_element" in the condition, get the elements and set them as objecttypes
+		objecttypes = ["node", "way", "relation", "area"]
+		if "conditions" in question:
+			if "osm_element" in question["conditions"][0]:
+				objecttypes = []
+				# get what type of data is stored at question["conditions"][0]["osm_element"]
+				if isinstance(question["conditions"][0]["osm_element"], str):
+					objecttypes.append(OpenStopTypeToTaginfoType(question["conditions"][0]["osm_element"]))
+				elif isinstance(question["conditions"][0]["osm_element"], list):
+					for t in question["conditions"][0]["osm_element"]:
+						objecttypes.append(OpenStopTypeToTaginfoType(t))
 
-for combikey, data in questions_for_tags.items():
-	key = combikey.split("=")[0]
-	value = combikey.split("=")[1]
-	AddToTags(key, value, data["elements"], getQuestionDescription(data["questions"]))
+		for answer in question["answer"]["input"]:
+			if "osm_tags" not in answer:
+				continue
+			for key, value in answer["osm_tags"].items():
+				AddToQuestionsForTags(key, value, str(question["question"]["text"]), objecttypes)
+
+	for combikey, data in questions_for_tags.items():
+		key = combikey.split("=")[0]
+		value = combikey.split("=")[1]
+		AddToTags(key, value, data["elements"], getQuestionDescription(data["questions"]))
 
 
-TAGINFO["tags"] = TAGS
+	TAGINFO["tags"] = TAGS
 
-# Delete the files we just downloaded
-os.remove(os.path.join(json_dir, "map_feature_collection.json"))
-os.remove(os.path.join(json_dir, "question_catalog.json"))
+	# Delete the files we just downloaded
+	os.remove(os.path.join(json_dir, "map_feature_collection.json"))
+	os.remove(os.path.join(json_dir, "question_catalog.json"))
 
-# BEFORE we write the taginfo.json file, we generate the md5 of the string representation of the TAGINFO dictionary
-# and add it to the dictionary
-new_md5 = hashlib.md5(json.dumps(TAGINFO, ensure_ascii=False).encode("utf8")).hexdigest()
+	# write the taginfo.json file
+	with open(os.path.join(json_dir, "taginfo.json"), "w", encoding="utf8") as f:
+		f.write(json.dumps(TAGINFO, indent=4, ensure_ascii=False))
 
-if new_md5 == previous_md5:
-	print("No changes detected, exiting")
-	exit()
+
+question_catalog_date = get_last_commit_date("OPENER-next/OpenStop", "assets/datasets/question_catalog.json")
+taginfo_date = get_last_commit_date("OPENER-next/OpenStop-taginfo", "taginfo.json")
+
+print("question_catalog.json was last updated on " + question_catalog_date)
+print("taginfo.json was last updated on " + taginfo_date)
+
+if question_catalog_date > taginfo_date:
+    print("The question_catalog.json file is more recent than the taginfo.json file.")
+    main()
 else:
-	print("Changes detected, writing taginfo.json")
-	TAGINFO["project"]["catalog-md5"] = new_md5
-	TAGINFO["data_updated"] = datetime.strftime(datetime.now(), '%Y%m%dT%H%M%SZ')
-
-
-# write the taginfo.json file
-with open(os.path.join(json_dir, "taginfo.json"), "w", encoding="utf8") as f:
-	f.write(json.dumps(TAGINFO, indent=4, ensure_ascii=False))
-
-
-
+    print("The taginfo.json file is more recent or equal to the question_catalog.json file.")
 
 
 
